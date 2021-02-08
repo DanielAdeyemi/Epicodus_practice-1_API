a4321e5f25418e48487d183793856084acef3eed
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-08 10:24:34 -0800

    add second url and call API depends on the button click
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>
    
    Co-authored-by: Woo Jin Kim <kimwoojin211@gmail.com>

--- src/js/main.js
+++ src/js/main.js
@@ -4,26 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '.././css/styles.css';
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    // const city = $('#location').val();
+  $('.btn').click(function() {
+    const city = $('#location').val();
     const zip = $('#zip').val();
     const country = $('#countryCode').val();
     $('#location, #zip, #countryCode').val("");
 
     let request = new XMLHttpRequest();
-    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${process.env.API_KEY}`;
+    const url1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url2 = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${process.env.API_KEY}`;
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
       }
     };
-    request.open("GET", url, true);
+
+    request.open("GET", ($(this).attr('id') === 'weatherLocation') ? url1 : url2, true);
     request.send();
 
     function getElements(response) {
-      $('.showHumidity').text(`The humidity for ${zip} is ${response.main.humidity}%`);
+      $('.showHumidity').text(`The humidity for ${zip}${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
       $('.description').text(`The clouds are: ${response.weather[0].description}`);
     }
