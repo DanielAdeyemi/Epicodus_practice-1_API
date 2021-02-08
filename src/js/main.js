75ad6bba591dbd365a58a14169fd7ca91d6effc9
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-07 18:06:18 -0800

    update functionality to make it template for upcoming projects

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import { Class } from './logic.js';
+import '.././css/styles.css';
+// import Name from './business_logic.js';
