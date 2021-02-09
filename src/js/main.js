dffc1f9032a87fc1bb0e63129a28356a4037f07e
DanielAdeyemi <adeyemidany@gmail.com>
2021-02-09 11:17:01 -0800

    update mainjs using promise
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>

--- src/js/main.js
+++ src/js/main.js
@@ -6,35 +6,30 @@ import '.././css/styles.css';
 $(document).ready(function() {
   $('.btn').click(function() {
     const city = $("#location").val();
-    try {
-      if (city === "") {
-        throw 'blankException';
-      }
-    } catch (error) {
-      $(".showErrors").text(error);
-    }
     const zip = $("#zip").val();
     const country = $("#countryCode").val();
     $("#location, #zip, #countryCode").val("");
 
-    let request = new XMLHttpRequest();
-    const url1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-    const url2 = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${process.env.API_KEY}`;
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+      const url2 = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${process.env.API_KEY}`;
 
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
-
-    request.open("GET", $(this).attr("id") === "weatherLocation" ? url1 : url2, true);
-    request.send();
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity for ${zip}${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-      $('.description').text(`The clouds are: ${response.weather[0].description}`);
-    }
+      request.onload = function() {
+        (this.status === 200) ? resolve(request.response): reject(request.response);
+      };
+      request.open("GET", $(this).attr("id") === "weatherLocation" ? url1 : url2, true);
+      request.send();
+    });
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity for ${zip}${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      $('.description').text(`The clouds are: ${body.weather[0].description}`);
+      $('.showErrors').text('');
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+      $('.showHumidity, .showTemp, .description').text('');
+    });
   });
 });
